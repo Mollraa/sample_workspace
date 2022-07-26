5114ba0b5d5f6c101af41b3ecec66694f3639d4f
admin <admin@YD-07>
2022-07-26 17:54:51 +0900

    726

--- Study/src/YD_homework/ex05.java
+++ Study/src/YD_homework/ex05.java
@@ -14,27 +14,40 @@ public class ex05 {
 		//	출력예시, "상품명 : 가격"
 		// 4) 분석기능은 최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총합을 구합니다.
 		// 5) 종료 시에는 프로그램을 종료한다고 메세지를 출력하도록 구현하세요.
-		boolean pro = true; //while문 실행변수
-		int num = 0; //상품수
-		int[] pri = null; //상품가격
-		String name = null; //상품값
-		String[] name2 = null;
 		Scanner scn = new Scanner(System.in); //입력값 받겠습니다
+		boolean pro = true; //while문 실행변수
+		int num = 0; //상품수받을때 변수선언
+		int[] pri = null; //상품가격 저장을 위한 배열
+		String[] name = null; //상품값입력을 위한 배열
+		int max = 0;
+		int sum = 0;
 		
 		while(pro) {
 			System.out.println("1.상품 수 | 2.상품 및 가격입력 | 3.제품별 가격 | 4.분석 | 5.종료");
 			System.out.println("---------------------------------------------------------");
-			int selectNo = scn.nextInt(); //목록에서 받을 번호값
+			System.out.println("선택 > ");
+			int selectNo = Integer.parseInt(scn.nextLine()); //목록에서 받을 번호값
 			
 			switch(selectNo) {
 			case 1:
 				System.out.println("상품 수 입력해주세요: ");
 				num = scn.nextInt();
 				pri = new int[num];
+
+break;
 			case 2:
 				System.out.println("상품을 입력해 주세요:");
-				String name = scn.nextLine();
+				for(int i=0 i<)
+			case 3:
+				break;
 			
+			case 4:
+				break;
+				
+			case 5:
+				System.out.println("프로그램 종료합니다.");
+				break;
+				
 			}
 		}
 		
